dd95df28eefd74729a82b9da2dcf46c3cdf35556
Mada2150 <90259162+AdamHallengreen@users.noreply.github.com>
2025-03-23 21:13:56 -0700

    refactor: combine plot functions

--- src/produce_figures.py
+++ src/produce_figures.py
@@ -5,7 +5,7 @@ import json
 import yaml
 
 from figure_manager import FigureManager
-from figure_manager import generate_plot, generate_plot_with_error
+from figure_manager import generate_plot
 from utils.find_project_root import find_project_root
 
 def make_figures(EXTERNAL_DATA_PATH, FIGURES_DIR, PAPER_SIZE, FILE_EXT, USE_LATEX, VERBOSE):
@@ -19,36 +19,40 @@ def make_figures(EXTERNAL_DATA_PATH, FIGURES_DIR, PAPER_SIZE, FILE_EXT, USE_LATE
     
     # make figure 2
     fm = FigureManager(output_dir=PROJECT_ROOT / FIGURES_DIR, paper_size=PAPER_SIZE, file_ext=FILE_EXT, use_latex=USE_LATEX)
-    make_figure_2(fm, data, VERBOSE)
+    make_figure_2(fm, data)
     
 def make_figure_1(fm, data, VERBOSE):
         # Create figure and subplot managers with specified layout
     fig, axes = fm.create_figure(n_rows=2, n_cols=2, n_subplots=3)
 
-    axes[0] = generate_plot(data, x_col='school', y_col='wage', plot_type='plot', x_bins=5, group_by_cols='residence', agg_fct=pl.mean, ax = axes[0], verbose=VERBOSE, xlabel='School', ylabel='Wage', title='Average Wage by School and Residence')
-    axes[1] = generate_plot(data, x_col='exper', y_col='wage', plot_type='plot', x_bins=5, group_by_cols='residence', agg_fct=pl.mean, ax = axes[1], verbose=VERBOSE, xlabel='Experience', ylabel='Wage', title='Average Wage by Experience and Residence')
-    axes[2] = generate_plot(data, x_col='wage', bins=30, plot_type='hist', group_by_cols='maried', ax = axes[2], verbose=VERBOSE, plot_settings={'alpha': 0.5})
+    axes[0] = generate_plot(data, x='school', y='wage', plot_type='plot', x_bins=5, group_by='residence', agg_fct=pl.mean, ax = axes[0], verbose=VERBOSE, xlabel='School', ylabel='Wage', title='Average Wage by School and Residence')
+    axes[1] = generate_plot(data, x='exper', y='wage', plot_type='plot', x_bins=5, group_by='residence', agg_fct=pl.mean, ax = axes[1], verbose=VERBOSE, xlabel='Experience', ylabel='Wage', title='Average Wage by Experience and Residence')
+    axes[2] = generate_plot(data, x='wage', bins=30, plot_type='hist', group_by='maried', ax = axes[2], verbose=VERBOSE, plot_settings={'alpha': 0.5})
 
     # Save the entire figure and subplots
     fm.save_figure(filename="three_small_plots")
     
-def make_figure_2(fm, data, VERBOSE):
+def make_figure_2(fm, data):
 
     cd = collapsed_data = data.group_by('school').agg(
         pl.mean('wage').alias('wage_mean'),
         pl.std('wage').alias('wage_std'),
+        pl.mean('exper').alias('exper_mean'),
+        pl.std('exper').alias('exper_std'),
     ).with_columns(
         (c.wage_mean - c.wage_std*1.96).alias('wage_ci_low'),
         (c.wage_mean + c.wage_std*1.96).alias('wage_ci_high'),
+        (c.exper_mean - c.exper_std*1.96).alias('exper_ci_low'),
+        (c.exper_mean + c.exper_std*1.96).alias('exper_ci_high'),
     ).sort('school')
 
     # Create figure and subplot managers with specified layout
     fig, axes = fm.create_figure(n_rows=1, n_cols=2, n_subplots=2)
 
-    axes[0] = generate_plot_with_error(x=cd.get_column("school"), y=cd.get_column("wage_mean"),   y_err=cd.get_column("wage_std"),   ax = axes[0], xlabel='School', ylabel='Wage', title='Average Wage by School and Residence', label='Wage')
-    axes[0] = generate_plot_with_error(x=cd.get_column("school"), y=cd.get_column("wage_mean")*2, y_err=cd.get_column("wage_std"), ax = axes[0], xlabel='School', ylabel='Wage', title='Average Wage by School and Residence', label='Double Wage')
-    axes[1] = generate_plot_with_error(x=cd.get_column("school"), y=cd.get_column("wage_mean"),   y_err=(cd.get_column("wage_ci_low"),   cd.get_column("wage_ci_high")),   ax = axes[1], xlabel='School', ylabel='Wage', title='Average Wage by School and Residence', label="Wage")
-    axes[1] = generate_plot_with_error(x=cd.get_column("school"), y=cd.get_column("wage_mean")*2, y_err=(cd.get_column("wage_ci_low")*2, cd.get_column("wage_ci_high")*2), ax = axes[1], xlabel='School', ylabel='Wage', title='Average Wage by School and Residence', label="Double Wage")
+    axes[0] = generate_plot(cd, x="school", y="wage_mean", y_err="wage_std", ax = axes[0], xlabel='School years', ylabel='Wage', title='Average Wage by School and Residence', label='Wage')
+    axes[0] = generate_plot(cd, x="school", y="exper_mean", y_err="exper_std", ax = axes[0], xlabel='School years', ylabel='Experience', title='Average Experience by School and Residence', label='Experience')
+    axes[1] = generate_plot(cd, x="school", y="wage_mean", y_err=("wage_ci_low", "wage_ci_high"), ax = axes[1], xlabel='School years', ylabel='Wage', title='Average Wage by School and Residence', label="Wage")
+    axes[1] = generate_plot(cd, x="school", y="exper_mean", y_err=("exper_ci_low", "exper_ci_high"), ax = axes[1], xlabel='School years', ylabel='Experience', title='Average Experience by School and Residence', label="Experience")
     
     # Save the entire figure and subplots
     fm.save_figure(filename="two_std_dev_plots")
